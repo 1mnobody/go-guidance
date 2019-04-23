c83b82120796b7d82671d9cda5f9d0f8cec86a30
wuzhangsheng <dasheng253@163.com>
2019-04-23 10:52:02 +0800

    chore(map)：增加map定义

--- src/basic/map.go
+++ src/basic/map.go
@@ -3,13 +3,34 @@ package basic
 import "fmt"
 
 var m map[int]string
+
+// map的定义
+// map[key 类型]value类型 {
+//     key: value 的类型{ // 类型可省略
+//         value
+//     },   // 逗号是必须的
+// }
 var m2 = map[int]string{
 	1: "aa",
 	2: "bb",
 	3: "cc",
 }
 
-func MapDemo()  {
+var m3 = map[string]A{
+	"a": A{
+		1, 1,
+	},
+	"b": A{
+		2, 2,
+	},
+}
+
+var m4 = map[string]A{
+	"a": {2, 2},
+	"b": {3, 3},
+}
+
+func MapDemo() {
 	// 映射的零值为nil，不能添加键值对，以下语句会报错
 	//m[0] = "haha"
 
